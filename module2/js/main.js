5e0161b0994c3ed82b25d2dc816c089471dab389
Alvaro Garzón <alvaro.develops@gmail.com>
2022-10-04 10:31:06 +0200

    feat: scrollbar & return to top btn, done

--- module2/js/main.js
+++ module2/js/main.js
@@ -3,4 +3,30 @@
 document.querySelector(".burger_btn").addEventListener("click", (e) => {
     document.querySelector(".nav_links").classList.toggle("nav_links_hidden");
     e.target.classList.toggle("close_btn")
+});
+
+//Scrollbar horizontal progresivo
+const scrollbar = document.getElementById("scrollbar");
+const body = document.querySelector("body");
+
+const scrolling = () => {
+    const scrollPercentage = Math.round((window.scrollY / (body.offsetHeight - window.innerHeight)) * 100);
+    scrollbar.style.width = scrollPercentage + '%';
+    if(scrollPercentage < 0) {
+        scrollbar.style.width = "0%";
+    }
+};
+window.addEventListener("scroll", scrolling)
+
+
+//Return to top
+const returnToTop = document.querySelector(".return_top");
+
+returnToTop.addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 200)
 })
