3f06866b1a01b40568707ef9263a77b2899f95e0
Alvaro Garzón <alvaro.develops@gmail.com>
2022-10-05 11:50:59 +0200

    feat: validation name+email done, restructure style folder

--- module2/js/main.js
+++ module2/js/main.js
@@ -1,3 +1,4 @@
+import REGEX from "../utils/constants.js";
 
 //Toggle menu de header
 document.querySelector(".burger_btn").addEventListener("click", (e) => {
@@ -7,26 +8,66 @@ document.querySelector(".burger_btn").addEventListener("click", (e) => {
 
 //Scrollbar horizontal progresivo
 const scrollbar = document.getElementById("scrollbar");
-const body = document.querySelector("body");
+const body = document.querySelector(".body");
 
 const scrolling = () => {
     const scrollPercentage = Math.round((window.scrollY / (body.offsetHeight - window.innerHeight)) * 100);
     scrollbar.style.width = scrollPercentage + '%';
-    if(scrollPercentage < 0) {
-        scrollbar.style.width = "0%";
-    }
 };
 window.addEventListener("scroll", scrolling)
 
 
 //Return to top
-const returnToTop = document.querySelector(".return_top");
-
-returnToTop.addEventListener("click", () => {
+const chevronToTop = document.querySelector(".return_top");
+chevronToTop.addEventListener("click", () => {
     setTimeout(() => {
         window.scroll({
             top: 0,
             behavior: "smooth",
         })
     }, 200)
-})
+});
+
+//Not working hide btn
+const buttonToTop = document.querySelector(".return_top_container");
+if (window.scrollY > 0){
+    buttonToTop.classList.toggle("show")
+}
+
+//Validacion de formulario
+const form = document.querySelector(".form");
+const inputs = document.querySelectorAll(".input_text");
+const name = document.querySelector(".name");
+const email = document.querySelector(".email");
+const errorName = document.querySelector(".error_message_name");
+const errorEmail = document.querySelector(".error_message_email");
+
+const validateForm = (e) => {
+    if(e.target.name === "name") {
+        if(REGEX.name.test(e.target.value)) {
+            errorName.style.visibility = "hidden";
+            name.classList.remove('error');
+            name.classList.add("correct");         
+        } else {
+            errorName.style.visibility = "visible";
+            name.classList.add('error'); 
+            name.classList.remove("correct");
+        }
+    }
+    if (e.target.name === "email") {
+        if (REGEX.email.test(e.target.value)) {
+            errorEmail.style.visibility = "hidden";
+            email.classList.remove('error');
+            email.classList.add("correct");
+        } else {
+            errorEmail.style.visibility = "visible";
+            email.classList.add('error');
+            email.classList.remove("correct");
+        }
+    }
+};
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validateForm);
+});
+
---
+++ module2/utils/constants.js
@@ -0,0 +1,8 @@
+//REGEX
+const REGEX = {
+    name: /^[a-zA-Z0-9]{2,100}$/,
+    email: /[a-zA-Z0-9]+@[a-z0-9]+\.[a-z]{2,4}/,
+    // /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+export default REGEX;
